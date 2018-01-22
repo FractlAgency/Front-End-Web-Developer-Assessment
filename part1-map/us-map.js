fe000d5655450433eff0225f07b2871583bb386d
KentShepard <kentshepard7@gmail.com>
2018-01-22 00:37:14 -0500

    Add us.json and refactor to create map with it instead

--- part1-map/us-map.js
+++ part1-map/us-map.js
@@ -1,27 +1,34 @@
-var svg = d3.select("body")
-  .append('svg')
-  .attr('width', 960)
-  .attr('height', 600);
+let width = 960;
+let height = 600;
 
-var path = d3.geoPath();
+let svg = d3.select("body")
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+let projection = d3.geoAlbersUsa()
+  .scale(1000)
+  .translate([width / 2, height / 2]);
+
+let path = d3.geoPath()
+  .projection(projection);
 
 d3.csv('data-states.csv', (error, data) => {
   if (error) throw error;
 
-  d3.json("https://d3js.org/us-10m.v1.json", function (error, us) {
+  d3.json("us.json", (error, us) => {
     if (error) throw error;
-
+    
     svg.append("g")
       .attr("class", "states")
       .selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
+      .data(topojson.feature(us, us.objects.usStates).features)
       .enter().append("path")
       .attr("d", path);
 
     svg.append("path")
       .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; })));
-  });
+      .attr("d", path(topojson.mesh(us, us.objects.usStates, function (a, b) { return a !== b; })));
 
-  console.log(data);
+  });
 });
