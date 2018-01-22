4bab38dba9aaf7fcede393348194f9cf527caf45
Brandon King <brandonking000@hotmail.com>
2018-01-22 14:19:24 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,56 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-})
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip();
+// })
 
 
 let populationData;
-
-d3.csv("https://raw.githubusercontent.com/FractlAgency/Front-End-Web-Developer-Assessment/master/data-states.csv",    function(error, data) {
-        if (error) throw error;
-        console.log(data);
-        populationData = data;
-
-
+d3.csv("https://raw.githubusercontent.com/FractlAgency/Front-End-Web-Developer-Assessment/master/data-states.csv", function(error, data) {
+    if (error) throw error;
+    console.log(data);
+    populationData = data;
 });
 
 
-let usMap = document.querySelector('svg');
-usMap.addEventListener('mouseover', (event) => {
-    if (event.target.tagName === 'path') {
-        let usState = event.target;
-        usState.style.fill = 'blue';
-        console.log(usState.id);
+// const usMap = document.querySelector('svg');
+// usMap.addEventListener('mouseover', (event) => {
+//     if (event.target.tagName === 'path') {
+//         let usState = event.target;
+//         usState.style.fill = 'blue';
+//         console.log(usState.id);
+//
+//         usState.addEventListener('mouseout', (event) => {
+//             usState.style.fill = '';
+//
+//         });
+//     }
+//
+// });
 
-        usState.addEventListener('mouseout', (event) => {
-            usState.style.fill = '';
-
-        });
+// return a decimal number representing a state's population percent
+const getPopulationPercent = (stateAbbv) => {
+    for (let i in populationData) {
+        if(populationData[i]['State Abbv'] === stateAbbv) {
+            return Number(populationData[i]['Percent of Population'].slice(0,2)) / 100;
+        }
     }
+}
+
+// return array of all states in svg element
+const stateList = document.querySelectorAll('path');
+
+// loop through each state image giving each a background color corresponding to their % of population data
+stateList.forEach((state) => {
+	if(state.id !== 'frames') {
+		state.style.fill = 'rgb(42,130,206)';
+        state.style.fillOpacity = getPopulationPercent(state.id);
+        console.log('changed the opacity');
+	}
 
 });
+
+// stateList.forEach((state) => {
+// 	if(state.id !== 'frames') {
+//         state.style.fillOpacity = getPopulationPercent(state.id);
+// 	}
+//
+// });
