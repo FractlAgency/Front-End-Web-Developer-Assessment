78b97f8594422f0721c501cbb3c2acb5eb87aac4
Brandon King <brandonking000@hotmail.com>
2018-01-22 17:27:19 -0500

    January 22, 2018
    -Fixed issue where the scripts were running before the .csv data had been loaded.

--- js/scripts.js
+++ js/scripts.js
@@ -4,49 +4,64 @@
 
 
 let populationData;
-d3.csv("https://raw.githubusercontent.com/FractlAgency/Front-End-Web-Developer-Assessment/master/data-states.csv", function(error, data) {
-    if (error) throw error;
-    console.log(data);
-    populationData = data;
-});
-
-
-// const usMap = document.querySelector('svg');
-// usMap.addEventListener('mouseover', (event) => {
-//     if (event.target.tagName === 'path') {
-//         let usState = event.target;
-//         usState.style.fill = 'blue';
-//         console.log(usState.id);
-//
-//         usState.addEventListener('mouseout', (event) => {
-//             usState.style.fill = '';
-//
-//         });
-//     }
-//
-// });
-
-// return a decimal number representing a state's population percent
-const getPopulationPercent = (stateAbbv) => {
-    for (let i in populationData) {
-        if(populationData[i]['State Abbv'] === stateAbbv) {
-            return Number(populationData[i]['Percent of Population'].slice(0,2)) / 100;
-        }
-    }
-}
+let dataIsReady = false;
 
 // return array of all states in svg element
 const stateList = document.querySelectorAll('path');
 
-// loop through each state image giving each a background color corresponding to their % of population data
-stateList.forEach((state) => {
-	if(state.id !== 'frames') {
-		state.style.fill = 'rgb(42,130,206)';
-        state.style.fillOpacity = getPopulationPercent(state.id);
-        console.log('changed the opacity');
-	}
 
+d3.csv("https://raw.githubusercontent.com/FractlAgency/Front-End-Web-Developer-Assessment/master/data-states.csv", function(error, data) {
+    if (error) throw error;
+    console.log(data);
+    populationData = data;
+    setColors();
 });
+
+
+const usMap = document.querySelector('svg');
+usMap.addEventListener('mouseover', (event) => {
+    if (event.target.tagName === 'path') {
+        let usState = event.target;
+        usState.style.fill = 'blue';
+        usState.addEventListener('mouseout', (event) => {
+        usState.style.fill = '';
+        });
+    }
+});
+
+// return a decimal number representing a state's population percent
+const getPopulationPercent = function(stateAbbv) {
+    for (const i in populationData) {
+        if(populationData[i]['State Abbv'] === stateAbbv) {
+            const percentage = parseInt(populationData[i]['Percent of Population'].slice(0,2)) / 100;
+            return percentage.toString();
+        }
+    }
+}
+
+
+
+// loop through each state image giving each a background color corresponding to their % of population data
+var setColors = function() {
+    stateList.forEach(function(state) {
+        if(state.id !== 'frames') {
+            state.style.fill = 'rgb(42,130,206)';
+            state.style.fillOpacity = getPopulationPercent(state.id);
+        }
+    });
+}
+
+
+
+
+
+
+
+// for(let i = 0; i < stateList.length; i++) {
+//     if (stateList[i].id !== 'frames') {
+//         stateList[i].style.fillOpacity = getPopulationPercent(stateList[i].id);
+//     }
+// }
 
 // stateList.forEach((state) => {
 // 	if(state.id !== 'frames') {
