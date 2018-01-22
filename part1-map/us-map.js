0e18865cf2e5b045bc2c846f420ad791c122a9c3
KentShepard <kentshepard7@gmail.com>
2018-01-22 01:09:56 -0500

    Render map with different corresponding shades of gray

--- part1-map/us-map.js
+++ part1-map/us-map.js
@@ -18,13 +18,24 @@ d3.csv('data-states.csv', (error, data) => {
 
   d3.json("us.json", (error, us) => {
     if (error) throw error;
-    
+
+    let allGeoStates = topojson.feature(us, us.objects.usStates).features;
+
+    data.forEach((state) => {
+      allGeoStates.forEach((geoState) => {
+        if (state['State Abbv'] === geoState.properties.STATE_ABBR) {
+          geoState.properties.info = state;
+        }
+      });
+    });
+
     svg.append("g")
       .attr("class", "states")
       .selectAll("path")
       .data(topojson.feature(us, us.objects.usStates).features)
       .enter().append("path")
-      .attr("d", path);
+      .attr("d", path)
+      .attr("fill",(d) => {return `hsl(0, 0%, ${d.properties.info['Percent of Population']})`}); 
 
     svg.append("path")
       .attr("class", "state-borders")
