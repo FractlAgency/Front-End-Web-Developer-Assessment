220ab3661e7488cbb1150e6cc2bae93f942610ef
bsubedi26 <bsubedi26@gmail.com>
2018-01-22 23:48:46 -0500

    added react tooltip & minor css style changes

---
+++ map/src/components/Tooltip.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
+const Tooltip = props => {
+	render() {
+		return (
+      <ReactTooltip effect="solid" />
+    )
+	}
+}
+
+export default Tooltip;
