06d599d8163708a242a8cc2b0954d5d9807732a3
bsubedi26 <bsubedi26@gmail.com>
2018-01-22 23:56:09 -0500

    added main App functionality & communication w/ components

--- map/src/App.js
+++ map/src/App.js
@@ -1,19 +1,82 @@
 import React, { Component } from 'react';
-import './App.css';
+import Tooltip from 'components/Tooltip';
+
+import './assets/App.css';
+import dataStates from './util/data-states';
+import USAMap from "components/UsaMap";
+import objectByState from "util/objectByState";
 
 class App extends Component {
+  state = {
+    showHover: {
+      active: false
+    },
+    color: ''
+  }
+
+  setColor(type) {
+    this.setState({ color: type });
+  }
+
+  handleClick = (event) => {
+    // alert(event.target.dataset.name);
+  };
+
+  handleHover = (state) => {
+    const allStates = objectByState(dataStates);
+    this.setState({
+      showHover: {
+        active: true,
+        [state]: true,
+        data: allStates[state]
+      }
+    });
+
+  }
+
+  renderOnHover = (data) => {
+    const upperCaseFirstLetter = data["State"].charAt(0).toUpperCase() + data["State"].slice(1);
+
+    return (
+      <div className="state-details">
+        <h5>State: {upperCaseFirstLetter}</h5>
+        <h5>Median Household Income: {data["Median Household Income"]}</h5>
+        <h5>Percent of Income: {data["Percent of Income"]}</h5>
+        <h5>Percent of Population: {data["Percent of Population"]}</h5>
+      </div>
+    )
+  }
+
+  renderNonSelected() {
+    return (
+      <div className="state-details">
+        <h5>State:</h5>
+        <h5>Median Household Income:</h5>
+        <h5>Percent of Income:</h5>
+        <h5>Percent of Population:</h5>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="root-container App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <Tooltip />
+          <div>
+            <button className="btn" onClick={this.setColor.bind(this, 'rainbow')}>Rainbow</button>
+            <button className="btn" onClick={this.setColor.bind(this, 'gradient')}>Gradient</button>
+          </div>
+          {this.state.showHover.active ? this.renderOnHover(this.state.showHover.data) : this.renderNonSelected()}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="map-container">
+          <USAMap colorType={this.state.color} handleHover={this.handleHover} onClick={this.handleClick} />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
--- map/src/components/Tooltip.js
+++ map/src/components/Tooltip.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactTooltip from 'react-tooltip';
 
 const Tooltip = props => {
-	render() {
-		return (
-      <ReactTooltip effect="solid" />
-    )
-	}
+	return (
+		<ReactTooltip effect="solid" />
+	)
 }
 
 export default Tooltip;
