9b225f2c5489934bbd9473f4e1da3461fb562d90
Brandon King <brandonking000@hotmail.com>
2018-01-27 12:33:25 -0500

    January 27, 2017
    -Began the refactoring of the javascript for functions setting the colors of state shapes, and collection information from the .csv file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,87 @@
 let populationData;
-let dataIsReady = false;
 
-// return array of all states in svg element
-const stateList = document.querySelectorAll('path');
+// array of elements representing all state shapes on the US map diagram
+const stateShapeList = document.querySelectorAll('path');
 
-const usaMap = document.querySelector('svg');
+// actual DC shape element
+const DcEl = document.querySelector('#DC2');
 
+// select dropdown menu
+const selectEl = document.querySelector('select');
 
+let tooltipName = document.querySelector('.js-tooltip-name');
+let percentOfPopulation = document.querySelector('.js-pop-percent');
+let medHouseIncome = document.querySelector('.js-med-house-income');
+let percentOfIncome = document.querySelector('.js-percent-income');
+
+let currentUsaState = stateShapeList[0];
+
+// parse data from .csv file and assign it to a variable
 d3.csv("https://raw.githubusercontent.com/FractlAgency/Front-End-Web-Developer-Assessment/master/data-states.csv", function(error, data) {
     if (error) throw error;
     console.log(data);
     populationData = data;
-    setColors();
+    setStateColors();
+    highlightInitialState();
 });
 
-
-const getStateInfo = function(stateAbbv) {
+// returns a particular statistic for a given state
+const getStateInfo = (stateAbbv, key) => {
+    let data;
     for (let i = 0; i < populationData.length; i++) {
         if (populationData[i]['State Abbv'] === stateAbbv) {
-            return populationData[i];
+            data = populationData[i][key];
         }
     }
+    switch (key) {
+        case 'Percent of Population':
+            data = Number(data.slice(0,2)) / 100;
+            break;
+        case 'Median Household Income':
+            data = data.split('').filter(char => char !== '$' && char !== ',').join('');
+            break;
+        case 'Percent of Income':
+            data = data.slice(0,1)
+            break;
+        default:
+            console.log('Could not find correct category case');
+    }
+    return data;
 }
 
-// usaMap.addEventListener('mouseover', function(event) {
-//     if (event.target.tagName === 'path') {
-//         let usState = event.target;
-//         usState.style.fill = 'blue';
-//         usState.addEventListener('mouseout', function(event) {
-//         usState.style.fill = '';
-//         });
-//     }
-// });
+// give each state shape element a fill color corresponding to their "% of population" data
+const setStateColors = () => {
+    stateShapeList.forEach(state => {
+        if(state.id !== 'frames') {
+            if (state.id === 'DC1') {
+                state = DcEl;
+                state.style.stroke = 'transparent';
+            }
+            else state.style.fillOpacity = (1 - getStateInfo(state.id, 'Percent of Population')).toString();
+            state.style.fill = 'rgb(25,170,217)';
+        }
+    });
+}
 
-let currentUsaState = stateList[0];
-const selectEl = document.querySelector('select');
+// highlights alaska state for when page loads
+const highlightInitialState = () => {
+    document.querySelector('#AK').style.stroke = '#d3d3d3';
+    document.querySelector('#AK').style.strokeWidth = '3';
+}
 
-document.querySelector('#AK').style.stroke = '#d3d3d3';
-document.querySelector('#AK').style.strokeWidth = '3';
-
+const updateTooltip = function(stateAbbv) {
+    tooltipName.textContent = getStateInfo(stateAbbv, 'State Abbv');
+    percentOfPopulation.textContent = getStateInfo(stateAbbv, 'Percent of Population');
+    medHouseIncome.textContent = getStateInfo(stateAbbv, 'Median Household Income');
+    percentOfIncome.textContent = getStateInfo(stateAbbv, 'Percent of Income');
+}
 
 // listen for change in value in select element
 selectEl.addEventListener('change', function() {
     // loop through elements to find state with id matching abbv chosen in select menu and highlight its border color
-    for (let i = 0; i < stateList.length; i++) {
+    for (let i = 0; i < stateShapeList.length; i++) {
         const dcEl = document.querySelector('#DC2');
-        if (stateList[i].id === selectEl.value) {
+        if (stateShapeList[i].id === selectEl.value) {
             // if (currentUsaState) {
             //     currentUsaState.style.stroke = '';
             //     currentUsaState.style.strokeWidth = '0';
@@ -58,10 +94,10 @@ selectEl.addEventListener('change', function() {
             dcEl.style.stroke = '';
             dcEl.style.strokeWidth = '0';
 
-            currentUsaState = stateList[i];
+            currentUsaState = stateShapeList[i];
             currentUsaState.style.stroke = '#d3d3d3';
             currentUsaState.style.strokeWidth = '3';
-            updateTooltip(stateList[i].id)
+            updateTooltip(stateShapeList[i].id)
 
         }
         else if (selectEl.value === 'DC') {
@@ -70,69 +106,7 @@ selectEl.addEventListener('change', function() {
             currentUsaState = dcEl;
             dcEl.style.stroke = '#fff';
             dcEl.style.strokeWidth = '3';
-            updateTooltip(stateList[i].id.slice(0,2));
+            updateTooltip(stateShapeList[i].id.slice(0,2));
         }
     }
 });
-
-
-// return a decimal number representing a state's population percent
-const getPopPercent = function(stateAbbv) {
-    for (const i in populationData) {
-        if(populationData[i]['State Abbv'] === stateAbbv) {
-            const percentage = parseInt(populationData[i]['Percent of Population'].slice(0,2)) / 100;
-            return (1 - percentage).toString();
-        }
-    }
-}
-
-
-
-// loop through each state image giving each a background color corresponding to their % of population data
-const setColors = function() {
-    stateList.forEach(function(state) {
-        if(state.id !== 'frames') {
-            state.style.fill = 'rgb(25,170,217)';
-            state.style.fillOpacity = getPopPercent(state.id);
-            if (state.id === 'DC1') {
-                const dcEl = state.nextElementSibling;
-                dcEl.style.fill = 'rgb(25,170,217)';
-                dcEl.style.fillOpacity = getPopPercent(state.id);
-                document.querySelector('#DC2').style.strokeWidth = '0';
-            }
-        }
-        // else if (state.id === ) {
-        //
-        // }
-    });
-}
-
-let tooltipName = document.querySelector('.js-tooltip-name');
-let percentOfPopulation = document.querySelector('.js-pop-percent');
-let medHouseIncome = document.querySelector('.js-med-house-income');
-let percentOfIncome = document.querySelector('.js-percent-income');
-
-const updateTooltip = function(stateAbbv) {
-    tooltipName.textContent = getStateInfo(stateAbbv)['State Abbv'];
-    percentOfPopulation.textContent = getStateInfo(stateAbbv)['Percent of Population'];
-    medHouseIncome.textContent = getStateInfo(stateAbbv)['Median Household Income'];
-    percentOfIncome.textContent = getStateInfo(stateAbbv)['Percent of Income'];
-}
-
-
-
-
-
-
-// for(let i = 0; i < stateList.length; i++) {
-//     if (stateList[i].id !== 'frames') {
-//         stateList[i].style.fillOpacity = getPopPercent(stateList[i].id);
-//     }
-// }
-
-// stateList.forEach((state) => {
-// 	if(state.id !== 'frames') {
-//         state.style.fillOpacity = getPopPercent(state.id);
-// 	}
-//
-// });
