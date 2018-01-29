82059fef6e376296cf8ecca660c2711355988156
Brandon King <brandonking000@hotmail.com>
2018-01-29 00:16:33 -0500

    Refactored javscript, fixed bug, and added functionality to Top 5 card
    -Refactored javascript related the highlighting of state elements, and updating the tooltip, among other things
    -Fixed bug where the information for DC wouldn't show up in the tooltip
    -Fixed bug where an abrupt change would happen when the user would select DC or select another state from the dropdown with DC already selected
    -Added functionality to the Top 5 section so that it will display the top 5 states for each category depending on the radio button that is selected.

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,18 @@ let populationData;
 const stateShapeList = document.querySelectorAll('path');
 
 // DC circle shape element
-const DcEl = document.querySelector('#DC2');
+const dcEl = document.querySelector('#DC2');
 
 // select dropdown menu
 const selectEl = document.querySelector('select');
 
+// variable for keeping track of the currently highlighted state
+let currentState;
+
+// elements for tooltip
 let tooltipName = document.querySelector('.js-tooltip-name');
 let percentOfPopulation = document.querySelector('.js-pop-percent');
 let medHouseIncome = document.querySelector('.js-med-house-income');
-// let percentOfIncome = document.querySelector('.js-percent-income');
 
 // data filter form
 const filterForm = document.querySelector('#data-filter');
@@ -21,17 +24,17 @@ const filterForm = document.querySelector('#data-filter');
 // text representing current filter for "Top 5 States" section
 let cardSubtitle = document.querySelector('.js-card-subtitle');
 
-let currentUsaState = stateShapeList[0];
+const firstCardItem = document.querySelector('.js-card-item-first');
+const secondCardItem = document.querySelector('.js-card-item-second');
+const thirdCardItem = document.querySelector('.js-card-item-third');
+const fourthCardItem = document.querySelector('.js-card-item-fourth');
+const fifthCardItem = document.querySelector('.js-card-item-fifth');
 
-// parse data from .csv file and assign it to a variable
-
-// d3.csv("https://raw.githubusercontent.com/FractlAgency/Front-End-Web-Developer-Assessment/master/data-states.csv", function(error, data) {
-//     if (error) throw error;
-//     console.log(data);
-//     populationData = data;
-//     setStateColors('Percent of Population');
-//     highlightInitialState();
-// });
+const firstCardValue = document.querySelector('.js-card-value-first');
+const secondCardValue = document.querySelector('.js-card-value-second');
+const thirdCardValue = document.querySelector('.js-card-value-third');
+const fourthCardValue = document.querySelector('.js-card-value-fourth');
+const fifthCardValue = document.querySelector('.js-card-value-fifth');
 
 d3.csv("https://raw.githubusercontent.com/brandnk/Front-End-Web-Developer-Assessment/master/data-states-new.csv", function(error, data) {
     if (error) throw error;
@@ -39,6 +42,7 @@ d3.csv("https://raw.githubusercontent.com/brandnk/Front-End-Web-Developer-Assess
     populationData = data;
     setStateColors('Percent of Population');
     highlightInitialState();
+    setTop5('Percent of Population');
 });
 
 // returns a particular statistic for a given state
@@ -55,35 +59,41 @@ const getStateInfo = (stateAbbv, key) => {
 // calculate a state shapes fill-opacity value for a given statistic
 const calculateOpacity = {
     percentOfPopulation: (stateAbbv, key) => {
-        console.log('calculating population percent')
-        console.log((1 - Number(getStateInfo(stateAbbv, key).slice(0,-1)) / 15).toString())
         return (1 - Number(getStateInfo(stateAbbv, key).slice(0,-1)) / 15).toString();
     },
     medHouseIncome: (stateAbbv, key) => {
-        // return (1 - Number(getStateInfo(stateAbbv, key).slice(0,1)) / 4).toString();
-        console.log('calculating med house income')
         const str = getStateInfo(stateAbbv, 'Median Household Income');
-
         const num = Number(str.split('').filter(char => char !== '$' && char !== ',').join(''));
-
-        // return 1 - (num / 76260)
-        console.log(1 - (num / 86260))
-        return 1 - (num / 86260)
-
-        // console.log((1 - Number(getStateInfo(stateAbbv, 'Median Household Income').split('').filter(char => char !== '$' && char !== ',').join(''))).toFixed(4))
-        // let num = (1 - Number(getStateInfo(stateAbbv, 'Median Household Income').split('').filter(char => char !== '$' && char !== ',').join('')) / 76260).toFixed(4);
-
+        return 1 - (num / 86260); // NH has greatest value at $76,260
     }
 };
 
-// give each state shape element a fill color corresponding to the current data filter
-const setStateColors = (statistic, color = 'rgb(28,192,245)') => {
+// highlights the input state's border
+const setBorderHighlight = (stateEl) => {
+    // if (!stateEl.style.stroke || stateEl.style.stroke === 'transparent') {
+    if (!Number(stateEl.style.strokeWidth)) {
+        stateEl.style.stroke = '#d3d3d3';
+        stateEl.style.strokeWidth = '3';
+    } else {
+        stateEl.style.stroke = 'transparent';
+        stateEl.style.strokeWidth = '0';
+    }
+}
+
+// highlights alaska state shape when page loads
+const highlightInitialState = () => {
+    currentState = document.querySelector('#AK');
+    setBorderHighlight(currentState);
+}
+
+// give each state shape element a fill color and opacity corresponding to the current data filter
+const setStateColors = (statistic, color = 'rgb(25,152,217)') => {
     stateShapeList.forEach(state => {
         if(state.id !== 'frames') {
             // select colors to represent "% of Population" filter
             if (statistic === 'Percent of Population') {
                 if (state.id === 'DC1') {
-                    state = DcEl;
+                    state = dcEl;
                     state.style.fillOpacity = calculateOpacity.percentOfPopulation('DC', statistic);
                 }
                 else state.style.fillOpacity = calculateOpacity.percentOfPopulation(state.id, statistic);
@@ -91,7 +101,7 @@ const setStateColors = (statistic, color = 'rgb(28,192,245)') => {
             // select colors to represent "% of Income" filter
             else if (statistic === 'Median Household Income') {
                 if (state.id === 'DC1') {
-                    state = DcEl;
+                    state = dcEl;
                     state.style.fillOpacity = calculateOpacity.medHouseIncome('DC', statistic);
                 }
                 else state.style.fillOpacity = calculateOpacity.medHouseIncome(state.id, statistic);
@@ -101,15 +111,7 @@ const setStateColors = (statistic, color = 'rgb(28,192,245)') => {
     });
 }
 
-// const test = () => {
-//     let totalIncome = 0;
-//     for (let i = 0; i < populationData.length; i++) {
-//         totalIncome += Number(populationData[i]['Median Household Income'].split('').filter(char => char !== '$' && char !== ',').join(''));
-//     }
-//     console.log(totalIncome)
-// }
-
-// change color theme of map after selecting a differnt radio button filter
+// change color theme of map after selecting a different radio button filter
 filterForm.addEventListener('change', (event) => {
     const radioBtn = event.target;
     cardSubtitle.textContent = radioBtn.nextElementSibling.textContent;
@@ -119,63 +121,83 @@ filterForm.addEventListener('change', (event) => {
     switch (color) {
         case 'blue':
             statistic = 'Percent of Population'
-            color = 'rgb(28,192,245)'; // original value: rgb(25,170,217)
+            color = 'rgb(25,152,217)'; // original value: rgb(25,170,217)
             break;
         case 'green':
             statistic = 'Median Household Income'
             color = 'rgb(102,255,179)'; // original value: rgb(71,255,179)
             break;
         default:
-            console.log('Could not find correct data color');
+            console.log('Could not find correct color');
     }
+    setTop5(statistic);
     setStateColors(statistic, color);
 });
 
-
-// highlights alaska state shape when page loads
-const highlightInitialState = () => {
-    document.querySelector('#AK').style.stroke = '#d3d3d3';
-    document.querySelector('#AK').style.strokeWidth = '3';
-}
-
 const updateTooltip = function(stateAbbv) {
-    tooltipName.textContent = getStateInfo(stateAbbv, 'State Abbv');
+    tooltipName.textContent = stateAbbv;
     percentOfPopulation.textContent = getStateInfo(stateAbbv, 'Percent of Population');
     medHouseIncome.textContent = getStateInfo(stateAbbv, 'Median Household Income');
-    // percentOfIncome.textContent = getStateInfo(stateAbbv, 'Median Household Income');
 }
 
-// listen for change in value in select element
+
+// listen for when user selects an option in dropdown menu to highlight correct state and update tooltip
 selectEl.addEventListener('change', function() {
-    // loop through elements to find state with id matching abbv chosen in select menu and highlight its border color
+    // loop through elements to find state with id matching abbv chosen in select menu and highlight its border
     for (let i = 0; i < stateShapeList.length; i++) {
-        const dcEl = document.querySelector('#DC2');
-        if (stateShapeList[i].id === selectEl.value) {
-            // if (currentUsaState) {
-            //     currentUsaState.style.stroke = '';
-            //     currentUsaState.style.strokeWidth = '0';
-            //     console.log('test')
-            //     dcEl.style.stroke = '';
-            //     dcEl.style.strokeWidth = '0';
-            // }
-            currentUsaState.style.stroke = 'transparent';
-            currentUsaState.style.strokeWidth = '0';
-            dcEl.style.stroke = '';
-            dcEl.style.strokeWidth = '0';
+        if (stateShapeList[i].id === selectEl.value || selectEl.value === 'DC') {
+            // remove any present border highlighting from previously selected state
+            setBorderHighlight(currentState);
 
-            currentUsaState = stateShapeList[i];
-            currentUsaState.style.stroke = '#d3d3d3';
-            currentUsaState.style.strokeWidth = '3';
-            updateTooltip(stateShapeList[i].id)
+            // enter details of newly selected state into tooltip
+            updateTooltip(selectEl.value);
 
-        }
-        else if (selectEl.value === 'DC') {
-            currentUsaState.style.stroke = '';
-            currentUsaState.style.strokeWidth = '';
-            currentUsaState = dcEl;
-            dcEl.style.stroke = '#fff';
-            dcEl.style.strokeWidth = '3';
-            updateTooltip(stateShapeList[i].id.slice(0,2));
+            // assign state element chosen by user to currentState variable
+            if (selectEl.value === 'DC') {
+                currentState = dcEl;
+            }
+            // if user didn't select DC
+            else currentState = stateShapeList[i];
+
+            // highlight the new state element's border
+            setBorderHighlight(currentState);
         }
     }
 });
+
+const setTop5 = (statistic) => {
+    const sortedArr = [];
+    for (let i = 0; i < populationData.length; i++) {
+        // add array to sortedArr with "% of population" value and state name
+        sortedArr.push([Number(populationData[i][statistic].split('').filter(char => char !== '$' && char !== ',' && char !== '%').join('')), populationData[i]['State']]);
+    }
+    // sort array in descending order (greatest to least)
+    sortedArr.sort((a,b) => {
+        return b[0] - a[0];
+    });
+
+    firstCardItem.textContent = sortedArr[0][1];
+    secondCardItem.textContent = sortedArr[1][1];
+    thirdCardItem.textContent = sortedArr[2][1];
+    fourthCardItem.textContent = sortedArr[3][1];
+    fifthCardItem.textContent = sortedArr[4][1];
+    if (statistic === 'Percent of Population') {
+        firstCardValue.textContent = `${sortedArr[0][0]}%`;
+        secondCardValue.textContent = `${sortedArr[1][0]}%`;
+        thirdCardValue.textContent = `${sortedArr[2][0]}%`;
+        fourthCardValue.textContent = `${sortedArr[3][0]}%`;
+        fifthCardValue.textContent = `${sortedArr[4][0]}%`;
+    }
+    else {
+        sortedArr.forEach((item, index, arr) => {
+            arr[index][0] = arr[index][0].toString();
+            arr[index][0] = arr[index][0].slice(0,2) + ',' + arr[index][0].slice(2);
+        });
+
+        firstCardValue.textContent = `$${sortedArr[0][0]}`;
+        secondCardValue.textContent = `$${sortedArr[1][0]}`;
+        thirdCardValue.textContent = `$${sortedArr[2][0]}`;
+        fourthCardValue.textContent = `$${sortedArr[3][0]}`;
+        fifthCardValue.textContent = `$${sortedArr[4][0]}`;
+    }
+}
