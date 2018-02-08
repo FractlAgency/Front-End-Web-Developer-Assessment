cc97a4c80afe5b17cc1e06418e3e214a53c0662a
Brandon King <brandonking000@hotmail.com>
2018-02-07 21:35:30 -0500

    Added breakpoints for larger screen widths, gender buttons given functionality
    - User can now switch between the data for the men and the women by clicking on the corresponding button
    - Project should now look at least decent on larger screen sizes
    
    *problem occurred when trying to float the container holding the graphs*

--- js/piechart-scripts.js
+++ js/piechart-scripts.js
@@ -1,9 +1,11 @@
 let genderData;
+const filterContainer = document.querySelector('.filter');
+const graphList = document.querySelectorAll('.graph');
+
 d3.csv("https://raw.githubusercontent.com/brandnk/Front-End-Web-Developer-Assessment/master/data-gender-age.csv", function(error, data) {
     if (error) throw error;
     console.log(data);
     genderData = data;
-
 
     const ctx1 = document.getElementById('chart1').getContext('2d');
     let chart1 = new Chart(ctx1, {
@@ -32,8 +34,6 @@ d3.csv("https://raw.githubusercontent.com/brandnk/Front-End-Web-Developer-Assess
         }
     });
 
-
-
     const ctx2 = document.getElementById('chart2').getContext('2d');
     let chart2 = new Chart(ctx2, {
         // The type of chart we want to create
@@ -60,10 +60,81 @@ d3.csv("https://raw.githubusercontent.com/brandnk/Front-End-Web-Developer-Assess
             }
         }
     });
+
+    const ctx3 = document.getElementById('chart3').getContext('2d');
+    let chart3 = new Chart(ctx3, {
+        // The type of chart we want to create
+        type: 'doughnut',
+
+        // The data for our dataset
+        data: {
+            labels: ["Ready", "Somewhat Ready", "Not Ready"],
+            datasets: [{
+                label: "Gender Preparedness",
+                backgroundColor: ['#FF6083', '#3BA5EC', '#FFCB52'],
+                borderColor: '#fff',
+                data: [getValue(0, 'Percent Value'),
+                        getValue(1, 'Percent Value'),
+                        getValue(2, 'Percent Value')]
+            }]
+        },
+        // Configuration options go here
+        options: {
+            legend: {
+                labels: {
+                    fontColor: "#fff"
+                }
+            }
+        }
+    });
+
+    const ctx4 = document.getElementById('chart4').getContext('2d');
+    let chart4 = new Chart(ctx4, {
+        // The type of chart we want to create
+        type: 'doughnut',
+
+        // The data for our dataset
+        data: {
+            labels: ["Ready", "Somewhat Ready", "Not Ready"],
+            datasets: [{
+                label: "Gender Preparedness",
+                backgroundColor: ['#FF6083', '#3BA5EC', '#FFCB52'],
+                borderColor: '#fff',
+                data: [getValue(3, 'Percent Value'),
+                        getValue(4, 'Percent Value'),
+                        getValue(5, 'Percent Value')]
+            }]
+        },
+        // Configuration options go here
+        options: {
+            legend: {
+                labels: {
+                    fontColor: "#fff"
+                }
+            }
+        }
+    });
 });
 
 
 const getValue = (objNum, key) => {
-    return genderData[objNum][key] * 100;
+    return genderData[objNum][key];
 
 }
+
+
+filterContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const gender = e.target.dataset.gender;
+        for (let i = 0; i < graphList.length; i++) {
+            if (graphList[i].dataset.gender === gender) {
+                graphList[i].style.display = 'block';
+            }
+            else {
+                graphList[i].style.display = 'none';
+            }
+        }
+
+    }
+
+});
